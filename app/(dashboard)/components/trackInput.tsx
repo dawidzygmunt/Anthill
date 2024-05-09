2bfc3eab6c93d0758164002603a22a0a6bbfbc3f
Marcin Konopka <marcin@brainhubteam.co>
2024-05-09 14:39:55 +0200

    Fix: Bug causing multiple fields to be filled at once

---
+++ app/(dashboard)/components/trackInput.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/use-toast"
+
+const FormSchema = z.object({
+  trackInput: z.number().min(1, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+const TrackInput = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {},
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onBlur={form.handleSubmit(onSubmit)}
+          className="space-y-6 justify-start"
+        >
+          <FormField
+            control={form.control}
+            name="trackInput"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </>
+  )
+}
+
+export default TrackInput
--- app/(dashboard)/components/tracks-row.tsx
+++ app/(dashboard)/components/tracks-row.tsx
@@ -1,72 +1,12 @@
-"use client"
+import { TracksProps } from "@/lib/types"
+import TrackInput from "./trackInput"
 
-import { TracksProps } from '@/lib/types'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
-
-
-const FormSchema = z.object({
-  trackInput1: z.number().min(1, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
-
-
-const TracksRow = ({
-  trackData
-}: { trackData: TracksProps[] }) => {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-
-    },
-  })
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    })
-  }
+const TracksRow = ({ trackData }: { trackData: TracksProps[] }) => {
   return (
     <>
       {trackData.map((track, index) => (
         <div key={index} className="text-center py-1 px-2">
-          <Form {...form}>
-            <form onBlur={form.handleSubmit(onSubmit)} className="space-y-6 justify-start">
-              <FormField
-                control={form.control}
-                name={`trackInput${index}`}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input placeholder="" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
+          <TrackInput />
         </div>
       ))}
     </>
