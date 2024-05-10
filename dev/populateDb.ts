fdc81cc147cd8269a0723e56c579d2df15e13da7
Marcin Konopka <marcin@brainhubteam.co>
2024-05-10 09:16:46 +0200

    Populating db with fixed data

--- dev/populateDb.ts
+++ dev/populateDb.ts
@@ -1,15 +1,14 @@
-import prisma from "../lib/db";
-import { faker } from "@faker-js/faker";
-import { Activity } from "@prisma/client";
-import { addDays, subDays, startOfWeek, setDefaultOptions } from "date-fns";
-import { clearDb } from "./clearDb";
+import prisma from "../lib/db"
+import { faker } from "@faker-js/faker"
+import { Activity } from "@prisma/client"
+import { addDays, subDays, startOfWeek, setDefaultOptions } from "date-fns"
 
-setDefaultOptions({ weekStartsOn: 1 });
+setDefaultOptions({ weekStartsOn: 1 })
 
 const randomActivities = (n = 100) => {
-  const names = Array.from(Array(n).keys()).map(() => faker.company.name());
-  return Array.from(new Set(names)).map((name) => ({ name }));
-};
+  const names = Array.from(Array(n).keys()).map(() => faker.company.name())
+  return Array.from(new Set(names)).map((name) => ({ name }))
+}
 
 const randomTracksForWeek = async (
   weekStart: Date,
@@ -17,13 +16,13 @@ const randomTracksForWeek = async (
 ) => {
   const days = Array.from(Array(5).keys()).map((shift) =>
     addDays(weekStart, shift)
-  );
+  )
 
   const randomActivities = await prisma.$queryRawUnsafe<Activity[]>(`
     SELECT * FROM public."Activity"
     ORDER BY RANDOM()
     LIMIT ${avgActivitiesPerWeek + Math.round(Math.random() * 10 - 0.5)};
-  `);
+  `)
 
   return days
     .map((day) => {
@@ -31,19 +30,18 @@ const randomTracksForWeek = async (
         date: day,
         activityId: activity.id,
         minutes: faker.number.int({ min: 60, max: 60 * 8 }),
-      }));
+      }))
     })
-    .flat();
-};
+    .flat()
+}
 
 //52 weeks in year
 const populateDb = async (weeks = 52 * 5) => {
-  await clearDb();
-  console.log("Populating db...");
-  const activities = randomActivities();
-  await prisma.activity.createMany({ data: activities });
+  console.log("Populating db...")
+  const activities = randomActivities()
+  await prisma.activity.createMany({ data: activities })
 
-  const weekStart = startOfWeek(new Date());
+  const weekStart = startOfWeek(new Date())
   const tracks = (
     await Promise.all(
       Array.from(Array(weeks).keys()).map(
@@ -51,9 +49,9 @@ const populateDb = async (weeks = 52 * 5) => {
           await randomTracksForWeek(subDays(weekStart, shift * 7))
       )
     )
-  ).flat();
+  ).flat()
 
-  await prisma.track.createMany({ data: tracks });
-};
+  await prisma.track.createMany({ data: tracks })
+}
 
-populateDb();
+populateDb()
