5ebd941cfe5eeca59df391b91d061a77b73cdded
Dawid <dawid@brainhubteam.co>
2024-05-24 10:58:27 +0200

    adding a random color when creating an activity

--- actions/post-activities.ts
+++ actions/post-activities.ts
@@ -3,6 +3,7 @@
 import prismaCodesMap from "@/app/(routes)/settings/utils/prismaCodes"
 import prisma from "@/lib/db"
 import { ActivitiesProps } from "@/lib/types"
+import { getRandomHexColor } from "@/lib/utils"
 import { z } from "zod"
 
 const activitySchema = z.object({
@@ -13,14 +14,15 @@ const activitySchema = z.object({
 })
 
 export const postActivities = async (data: ActivitiesProps) => {
+  const randomColor = getRandomHexColor()
   try {
     const parsedData = activitySchema.parse(data)
     const activity = await prisma.activity.create({
-      data: { name: parsedData.name },
+      data: { name: parsedData.name, color: randomColor },
     })
     return activity
   } catch (err: any) {
-    if ("errors" in err && err.errors.lenght > 0)
+    if ("errors" in err && err.errors.length > 0)
       return { error: err.errors[0].message }
     if ("code" in err && err.code in prismaCodesMap) {
       return {
--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,14 @@ export const timeFormatter = (minutes: number) => {
 
   return formattedTime
 }
+
+export const getRandomHexColor = () => {
+  const letters = "0123456789ABCDEF"
+  let color = "#"
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+
+  return color
+}
