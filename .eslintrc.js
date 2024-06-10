a5953abcaca1070712c7cacff1c8769686bdf5e8
Dawid <dawid@brainhubteam.co>
2024-06-10 16:15:02 +0200

    plugin remove

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,20 +8,9 @@ module.exports = {
     node: true,
   },
   extends: ["next/core-web-vitals"],
-  plugins: ["my-lint", "unused-imports"],
+  plugins: ["my-lint"],
   rules: {
     // "my-lint/func-prefix-matching": [1, { include: [], exclude: [] }],
     "my-lint/enforce-page-metadata-format": [1, { include: [], exclude: [] }],
-    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
   },
 }
