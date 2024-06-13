2eecdae75984f12cbde82926a811a4671067adb6
Dawid <dawid@brainhubteam.co>
2024-06-13 12:44:04 +0200

    time tracking in hours

--- app/(dashboard)/components/track-input.tsx
+++ app/(dashboard)/components/track-input.tsx
@@ -34,16 +34,36 @@ interface Props {
   track: Track | EmptyTrack
 }
 
+const myParse = (number: string) => {
+  const result = number.replace(",", ".")
+  const hours = parseFloat(result)
+  const minutes = hours * 60
+
+  const integerPart = Math.floor(hours)
+  const decimalPart = hours - integerPart
+
+  if (decimalPart >= 0.1 && decimalPart <= 0.5) {
+    return minutes + 30 - decimalPart * 60
+  } else if (decimalPart >= 0.6 && decimalPart <= 0.9) {
+    return Math.floor(minutes + 60 - decimalPart * 60)
+  } else {
+    return minutes - decimalPart * 60
+  }
+}
+
 const TrackInput = ({ track }: Props) => {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      trackInput: track.minutes?.toString() ?? "",
+      trackInput: track.minutes ? (track.minutes / 60).toString() : "",
     },
   })
 
   useEffect(() => {
-    form.setValue("trackInput", track.minutes?.toString() ?? "")
+    form.setValue(
+      "trackInput",
+      track.minutes ? (track.minutes / 60).toString() : ""
+    )
   }, [track, form])
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
@@ -51,23 +71,31 @@ const TrackInput = ({ track }: Props) => {
       const result = await deleteTrack(track.id)
       if ("error" in result) {
         DisplayError(result.error)
-        return form.setValue("trackInput", track.minutes?.toString() ?? "")
+        return form.setValue(
+          "trackInput",
+          track.minutes ? (track.minutes / 60).toString() : ""
+        )
       }
       return revalidateTracks()
     }
 
     if (data.trackInput.length < 1) return
 
+    const minutes = parseInt(myParse(data.trackInput).toString(), 10)
+
     const result = await handleTrackChange(
       track.trackRowId,
       track.date,
-      parseInt(data.trackInput, 10)
+      minutes
     )
     if ("error" in result) {
       DisplayError(result.error)
-      return form.setValue("trackInput", track.minutes?.toString() ?? "")
+      return form.setValue(
+        "trackInput",
+        track.minutes ? (track.minutes / 60).toString() : ""
+      )
     }
-    form.setValue("trackInput", result.minutes.toString())
+    form.setValue("trackInput", (result.minutes / 60).toString())
     revalidateTracks()
   }
   return (
